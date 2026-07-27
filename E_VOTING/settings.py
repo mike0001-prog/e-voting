237a927c1cdd2174abd7f80a26115b7c4ebc657c
oyeniyi kehinde <kehindeoyeniyi08@gmail.com>
2026-07-26 17:43:58 -0700

    updated settings.py

--- E_VOTING/settings.py
+++ E_VOTING/settings.py
@@ -14,16 +14,18 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g))=$ht7fjr+l8epggb110rz$bx=vye)j#-0o!03*jf)(1hdri'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
